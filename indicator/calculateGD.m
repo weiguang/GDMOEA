493b9a5f8fd3c15ae266c00aef22cb404a6deabd
weiguang <chen2621978@gmail.com>
2017-12-08 17:09:21 +0800

    增加了计算世代距离和反向世代距离

---
+++ indicator/calculateGD.m
@@ -0,0 +1,19 @@
+function GD = calculateGD(PF,fit)
+% calculate GD metric 
+% fit: popSize*M     
+% PF: pfSize*M
+
+[popSize, M]= size(fit);  
+pfSize =  size(PF,1);
+
+for i=1:popSize
+  for j=1:pfSize
+      D(i,j) = norm(fit(i,:)-PF(j,:),2); 
+  end
+  Dmin(i,:) = min(D(i,:));   
+end
+
+GD = sqrt(sum(Dmin.^2))/popSize;
+
+end
+
