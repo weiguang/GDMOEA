3402691813e20664ea72786e45dee92538151aff
weiguang <chen2621978@gmail.com>
2017-12-08 17:45:48 +0800

    修改了画图

--- plot/plotPF.m
+++ plot/plotPF.m
@@ -1,11 +1,19 @@
 function plotPF(PF)
 % plot 2D or 3D PF
 %
-    varSize = size(PF,2)
-    if varSize == 2
-        scatter(PF(:,1),PF(:,2))
-    elseif varSize == 3
-         scatter3(PF(:,1),PF(:,2),PF(:,3))
+    varSize = size(PF,2);
+    if varSize <= 3
+        if varSize == 2
+            %scatter(PF(:,1),PF(:,2))
+            plot(PF(:,1),PF(:,2),'bo','MarkerSize',6);
+        elseif varSize == 3
+             %scatter3(PF(:,1),PF(:,2),PF(:,3))
+              plot3(PF(:,1),PF(:,2),PF(:,3),'bo','MarkerSize',6);
+              zlabel('3^{nd} Objective');
+        end
+        xlabel('1^{st} Objective');
+        ylabel('2^{nd} Objective');
+        title('Non-dominated Solutions (F_{1})');
     else
         disp('Object size more than 3!')     
     end  
